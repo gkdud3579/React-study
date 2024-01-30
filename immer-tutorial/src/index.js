18ccf62b1f5374d3e91bf143f15c62fe74b1e454
Hayoung Shim <gkdud3579@gmail.com>
2024-01-30 17:13:17 +0900

    learned chapter 12 - immer library

--- immer-tutorial/src/index.js
+++ immer-tutorial/src/index.js
@@ -1,86 +1,17 @@
-import { useRef, useCallback, useState } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const App = () => {
-  const nextId = useRef(1);
-  const [form, setForm] = useState({ name: '', username: '' });
-  const [data, setData] = useState({
-    array: [],
-    uselessValue: null
-  });
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-  const onChange = useCallback(
-    e => {
-      const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: [value]
-      });
-    },
-    [form]
-  
-    )
-
-  const onSubmit = useCallback(
-    e => {
-      e.preventDefault();
-      const info = {
-        id: nextId.current,
-        name: form.name,
-        username: form.username
-      };
-
-      setData({
-        ...data,
-        array: data.array.concat(info)
-      });
-
-      setForm({
-        name: '',
-        username: ''
-      });
-      nextId.current += 1;
-    },
-    [data, form.name, form.username]
-  );
-
-  const onRemove = useCallback(
-    id => {
-      setData({
-        ...data,
-        array: data.array.filter(info => info.id !== id)
-      });
-    },
-    [data]
-  );
-
-  return (
-    <div>
-      <form on Submit={onSubmit}>
-        <input
-        name="username"
-        placeholder="아이디"
-        value={form.username}
-        onChange={onChange}
-        />
-        <input
-        name="name"
-        placeholder="이름"
-        value={form.name}
-        onChange={onChange}
-        />
-        <button type="submit">등록</button>
-        </form>
-        <div>
-          <ul>
-            {data.array.map(info => (
-              <li key={info.id} onClick={() => onRemove(info.id)}>
-                {info.username} ({info.name})
-              </li>
-            ))}
-          </ul>
-        </div>
-    </div>
-  );
-};
-
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
