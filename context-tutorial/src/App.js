7dcd0c8f297fe2caef8ce7d623ec32e8edcda3dc
Hayoung Shim <gkdud3579@gmail.com>
2024-02-04 18:01:02 +0900

    learned chapter15 - Context API

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import ColorBox from "./components/ColorBox";
+import { ColorProvider } from "./contexts/color";
+import SelectColor from "./components/SelectColors";
+
+const App = () => {
+  return (
+    <ColorProvider>
+      <div>
+        <SelectColor />
+        <ColorBox />
+      </div>
+    </ColorProvider>
+  );
+};
+
+export default App;
